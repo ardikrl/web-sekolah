308b37292a557f07a928cf2b6260897a75679c2c
root <root@ip-172-31-60-134.us-west-2.compute.internal>
2020-12-11 08:45:23 +0000

    Update default field for staff

--- sekolah/models.py
+++ sekolah/models.py
@@ -108,7 +108,7 @@ class Guru(models.Model):
 
 class Staff(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    gelar = models.CharField(max_length=7, blank=True, null=True, default='S.Pd.')
+    gelar = models.CharField(max_length=7, blank=True, null=True)
     pas_photo = models.ImageField(upload_to="photo-guru", blank=True, null=True)
     nip = models.CharField(max_length=50, blank=True, null=True)
     bio = models.TextField(max_length=500, blank=True, null=True)
